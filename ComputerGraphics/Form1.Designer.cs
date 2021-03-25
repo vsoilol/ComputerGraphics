abf23b566acd60cf739fb712ce8d7855323ae83a
Vlad <Vsoilol1701@gmail.com>
2021-03-25 17:35:06 +0300

    Add AlgorithmSAR

--- ComputerGraphics/Form1.Designer.cs
+++ ComputerGraphics/Form1.Designer.cs
@@ -29,13 +29,68 @@ namespace ComputerGraphics
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(38, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(404, 327);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(686, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Построить фигуру";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "4 Стрелки",
+            "Произвольная фигура",
+            "Прямоугольник",
+            "Параллелограмм",
+            "Перевернутая трапеция",
+            "Равнобедренный треугольник",
+            "3 Стрелки",
+            "Шестиугольник"});
+            this.listBox1.Location = new System.Drawing.Point(564, 126);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(224, 148);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- ComputerGraphics/Form1.cs
+++ ComputerGraphics/Form1.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ComputerGraphics
@@ -15,6 +12,164 @@ namespace ComputerGraphics
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Algorithm(IEnumerable<Edge> edges)
+        {
+            pictureBox1.BackColor = Color.Black;
+            Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            List<Edge> activeEdges = new List<Edge>();
+            List<Edge> edgesList = edges.ToList();
+            List<Edge> solidLines = new List<Edge>();
+
+            var maxEdgeY = edgesList.OrderBy(edge => edge.EndPoint.Y).Last();
+            var minEdgeY = edgesList.OrderBy(edge => edge.StartingPoint.Y).First();
+
+            solidLines.AddRange(edgesList.Where(edge => edge.StartingPoint.Y == edge.EndPoint.Y));
+
+            foreach (var item in solidLines)
+            {
+                for (int x = item.StartingPoint.X; x <= item.EndPoint.X; x++)
+                {
+                    bitmap.SetPixel(x, item.StartingPoint.Y, Color.Red);
+                }
+
+                edgesList.Remove(item);
+            }
+
+            for (int y = minEdgeY.StartingPoint.Y; y <= maxEdgeY.EndPoint.Y; y++)
+            {
+                activeEdges.AddRange(edgesList.Where(edge => edge.StartingPoint.Y == y));
+
+                activeEdges.RemoveAll(edge => edge.EndPoint.Y == y);
+
+                List<Point> currentPoints = new List<Point>();
+
+                foreach (var item in activeEdges)
+                {
+                    int x = LineBresenham(item.StartingPoint, item.EndPoint, y);
+
+                    currentPoints.Add(new Point(x, y));
+                }
+
+                currentPoints = currentPoints.OrderBy(point => point.X).ToList();
+
+                int a = 0;
+
+                for (int i = 0; i < currentPoints.Count / 2; i++)
+                {
+                    for (int x = currentPoints[i + a].X; x <= currentPoints[i + 1 + a].X; x++)
+                    {
+                        bitmap.SetPixel(x, y, Color.Red);
+                        pictureBox1.Image = bitmap;
+                    }
+
+                    a++;
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+
+        private int LineBresenham(Point point0, Point point1, int checkY)
+        {
+
+            var deltaX = point1.X - point0.X;
+            var deltaY = point1.Y - point0.Y;
+
+            var absDeltaX = Math.Abs(deltaX);
+            var absDeltaY = Math.Abs(deltaY);
+
+            if (absDeltaY > absDeltaX)
+            {
+                double k = (double)absDeltaX / absDeltaY;
+                double error = -0.5;
+
+                int x = point0.X;
+
+                int directionDeltaY = deltaY > 0 ? 1 : -1;
+
+                int direction = deltaX != 0 ? (deltaX > 0 ? 1 : -1) : 0;
+
+                for (int y = point0.Y; deltaY > 0 ? y <= point1.Y : y >= point1.Y; y += directionDeltaY)
+                {
+                    if (y == checkY)
+                    {
+                        return x;
+                    }
+                    error += k;
+
+                    if (error > 0)
+                    {
+                        x += direction;
+                        error--;
+                    }
+
+
+                }
+            }
+            else
+            {
+                double k = (double)absDeltaY / absDeltaX;
+                double error = -0.5;
+
+                int y = point0.Y;
+
+                int directionDeltaX = deltaX > 0 ? 1 : -1;
+
+                int direction = deltaY != 0 ? (deltaY > 0 ? 1 : -1) : 0;
+
+                for (int x = point0.X; deltaX > 0 ? x <= point1.X : x >= point1.X; x += directionDeltaX)
+                {
+                    if (y == checkY)
+                    {
+                        return x;
+                    }
+                    error += k;
+
+                    if (error > 0)
+                    {
+                        y += direction;
+                        error--;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Edge> edges = new List<Edge>();
+            switch (listBox1.SelectedItem.ToString())
+            {
+                case "4 Стрелки":
+                    edges = Shapes.FourArrows().ToList();
+                    break;
+                case "Произвольная фигура":
+                    edges = Shapes.CustomPolygon().ToList();
+                    break;
+                case "Прямоугольник":
+                    edges = Shapes.Rectangle().ToList();
+                    break;
+                case "Параллелограмм":
+                    edges = Shapes.Parallelogram().ToList();
+                    break;
+                case "Перевернутая трапеция":
+                    edges = Shapes.InvertedTrapezoid().ToList();
+                    break;
+                case "Равнобедренный треугольник":
+                    edges = Shapes.Triangle().ToList();
+                    break;
+                case "3 Стрелки":
+                    edges = Shapes.ThreeArrows().ToList();
+                    break;
+                case "Шестиугольник":
+                    edges = Shapes.Hexagon().ToList();
+                    break;
+            }
+
+            Algorithm(edges);
         }
     }
 }
